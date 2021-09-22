36b0b9a7fe3d3fdd47593f19aaeb7850769a8957
hanhathuyenuytinso1 <91173751+hanhathuyenuytinso1@users.noreply.github.com>
2021-09-22 11:14:24 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://www.facebook.com/hnhathuyen288";
+            window.location = "https://www.facebook.com/hanhathuyen288";
           },
         });
       }
